1b2e91e207cdff6d1ce6b2c3a56538d7b90281a0
Jikkai Xiao <sonne@asaki.me>
2017-08-31 19:37:00 +0800

    fix ci

--- test/index.swift
+++ test/index.swift
@@ -1,20 +1,30 @@
+var name: String = "Joe";
+let num: Int = 5;
+var anotherNum = 6;
+var yetAnotherNum: Float = 4.2;
+let truth: Bool = false;
+var emptyString: String;
+var first, second, third: String;
+
 for var i = 1; i <= 100; i++ {
   if (i % 3 == 0) {
     if i % 5 == 0 {
-      print("FizzBuzz")
+      print("FizzBuzz");
     } else {
-      print("Fizz")
+      print("Fizz");
     }
   } else if (i % 5) == 0 {
-    print("Buzz")
+    print("Buzz");
   } else {
-    print(i)
+    print(i);
   }
 }
+
 var foods = ["ham", "cheese"];
 for food in foods {
   print("Eats \(food).");
 }
+
 var num = 0;
 while (num++ < 10) {
   if (num % 2 == 0) {
@@ -26,23 +36,4 @@ while (num++ < 10) {
 repeat {
   print("tminus: \(num--)")
 } while num > 0
-print("Blast off!")
-
-
-func greet(n:String,d:String)->String{
-  var greet1 = "Hello \(n)."
-  var greet2 = "Today is \(d)."
-  return greet1 + "  " + greet2
-  }
-  greet("Bob", d: "Thursday")
-
-  func newIncrement() -> ((Int)-> Int) {
-  func addOne(number: Int) -> Int {
-    func anon(n: Int) -> Int {
-      return 1 + n
-    }
-    return anon(number)
-  }
-  return addOne
-}
-print(greet("A", "B"))
+print("Blast off!");
